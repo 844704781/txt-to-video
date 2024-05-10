e4d110da2346b73ce511e7c214edc4974bbad61f
aidev001 <aidev001@jigezi.ai>
2024-05-10 11:15:31 +0800

    bugfix

--- processor/pika_txt_processor.py
+++ processor/pika_txt_processor.py
@@ -4,14 +4,20 @@ from processor.pika_abstract_processor import PikaAbstractProcessor
 
 from logger_config import logger
 
+
 class PikaTxtAbstractProcessor(PikaAbstractProcessor):
 
     def write(self, page):
         for num in range(1, 10):
             page.mouse.click(10, 10)
         explor_page = page.locator("xpath=//main//a[contains(@class,'font-extra-thick')][2]")
-        for i in range(1, 20):
-            explor_page.click()
+        i = 0
+        while i < 10:
+            try:
+                explor_page.click()
+            except Exception as e:
+                time.sleep(1)
+                continue
 
         seconds = self.get_seconds(page)
         logger.info(self.name + "当前余额:{}", seconds)
