501f96e0b5cbe27241f49a429a1d7df890f1d5bf
aidev001 <aidev001@jigezi.ai>
2024-05-11 15:38:16 +0800

    bugfix

--- processor/runway_abstract_processor.py
+++ processor/runway_abstract_processor.py
@@ -149,13 +149,23 @@ class RunWayAbstractParser(AbstractProcessor):
         # 点击按钮
         generate_button = page.locator(
             "xpath=//button[contains(@class,'ExploreModeGenerateButton__Button-sc-ny02we-0')]")
-        while True:
-            disabled = generate_button.evaluate('(element) => element.disabled')
-            if not disabled:
-                break
-            # logger.debug(f"当前{disabled}")
-            time.sleep(1)
 
-        # generate_button = page.locator('button:has-text("Generate 4s")')
-        # generate_button.wait_for(timeout=60000)
+        generate_button = page.locator('button:has-text("Generate 4s")')
+        err = None
+        try:
+            generate_button.wait_for(timeout=30000)
+        except Exception as e:
+            err = e
+        if err is not None:
+            i = 0
+            num = 10
+            while i < num:
+                logger.info(self.name + f"尝试获取点击按钮，剩余尝试次数:\t{num - i}")
+                disabled = generate_button.evaluate('(element) => element.disabled')
+                if not disabled:
+                    break
+                # logger.debug(f"当前{disabled}")
+                time.sleep(1)
+                i += 1
+
         generate_button.click()
