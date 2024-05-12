d08035079d6d6c2b9aff79bf0d8f112889e6a7e6
aidev001 <aidev001@jigezi.ai>
2024-05-12 13:41:15 +0800

    bugfix

--- processor/runway_img_processor.py
+++ processor/runway_img_processor.py
@@ -18,6 +18,13 @@ class RunWayImgParser(RunWayAbstractParser):
         logger.info(self.name + "开始提交图片")
         file_path = self.image
         page.locator("input[type='file']").set_input_files(file_path)
-        order_sent = page.locator("div[data-uploading='false']")
-        order_sent.wait_for(timeout=60000)
+        order_sent = page.locator("xpath=//div[contains(@class,'Container-sc-ajvl93-0')]")
+        i = 0
+        while i < 20:
+            uploading = order_sent.get_attribute('data-uploading', timeout=3000)
+            logger.info(self.name + f"当前图片状态:uploading:{uploading}")
+            if uploading == 'false':
+                break
+            time.sleep(5)
+            i += 1
         return True
